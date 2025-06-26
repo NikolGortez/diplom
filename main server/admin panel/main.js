919313eadb4bc85b1f2fcc95bbc283d464a450b6
Nikol Gortez <117678971+NikolGortez@users.noreply.github.com>
2025-06-27 00:51:23 +0300

    Add files via upload

---
+++ main server/admin panel/main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+
+  // ПРОПИСЫВАЕМ ПРАВИЛЬНО!
+  const indexHtml = path.resolve(__dirname, '../admin panel/build/index.html');
+  win.loadFile(indexHtml);
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
